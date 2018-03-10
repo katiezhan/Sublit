a21c7a3746b97eea8aafb7ffcfbdc0e977803428
aphsai <saksham.a@hotmail.ca>
2018-03-09 22:51:51 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SublitApp extends React.Component {
+  render() {
+    return(
+      <h1> Sublit </h1>
+    );
+  }
+}
+
+ReactDOM.render(<SublitApp />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+    rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+    }]
+},
+resolve: {
+    extensions: ['.js', '.jsx']
+},
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: './dist',
+    }
+};
